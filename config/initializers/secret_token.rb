94e7f816bbb34503ede3f74658b9270a6e439dfa
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-14 16:24:46 -0600

    [add] initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedSocial::Application.config.secret_key_base = 'd1339b0dc47de3e6fcd77c8953ca2c7ce84565873dce30283d648756023f75488797b9b4e9c70514c229676c7e78d2f33f5504eb0ee1c6a62a961e15256debc9'
