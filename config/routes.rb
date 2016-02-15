70ff019643c60bc3ee6a4d7ec00f295c00e73032
Julian Hernandez <julianhernandez19@gmail.com>
2016-02-14 22:17:07 -0600

    [add] generate CRUD status with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RedSocial::Application.routes.draw do
+  resources :statuses
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,7 @@ RedSocial::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resource :status  
+
 end
